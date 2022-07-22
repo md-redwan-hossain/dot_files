d457edaa24bcf9a5342d8fd2c879a25270fe29d5
redwan-hossain <redwan.eng.cou@outlook.com>
2022-07-22 18:36:50 +0600

    pilot

---
+++ js.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+npm init esnext -y 
+npm i cross-fetch 
+npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier @chainsafe/eslint-plugin-node @overlookmotel/eslint-config-node eslint-config-airbnb
