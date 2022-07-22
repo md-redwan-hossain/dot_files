afac92d3a68e02a07dbefaac066e09078715b51a
redwan-hossain <redwan.eng.cou@outlook.com>
2022-07-22 23:22:31 +0600

    added more settings

--- js.sh
+++ js.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-npm init esnext -y 
-npm i cross-fetch 
-npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier @chainsafe/eslint-plugin-node @overlookmotel/eslint-config-node eslint-config-airbnb
+npm init esnext -y
+npm i cross-fetch
+npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier eslint-config-airbnb eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-import eslint-plugin-jsx @chainsafe/eslint-plugin-node @overlookmotel/eslint-config-node
